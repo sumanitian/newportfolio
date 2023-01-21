31b91ba3b1bf4742e8f6d2734c6550f0ebad18d9
Suman Prasad <70560551+sumanitian@users.noreply.github.com>
2023-01-21 22:30:33 +0530

    Update quora.jsx

--- pages/quora.jsx
+++ pages/quora.jsx
@@ -31,7 +31,7 @@ const property = () => {
           It is a platform to ask questions and connect with people who contribute unique insights and quality answers. 
           This empowers people to learn from each other and to better understand the world. 
           Over 100 million people visit Quora every month, 
-          so it's no surprise that many people ask similarly worded questions. 
+          so it is no surprise that many people ask similarly worded questions. 
           Multiple questions with the same intent can cause seekers to spend more time finding the best answer to their question, and make writers feel they need to answer multiple versions of the same question. 
           Quora values canonical questions because they provide a better experience to active seekers and writers, and offer more value to both of these groups in the long term.
           Identify which questions asked on Quora are duplicates of questions that have already been asked. 
