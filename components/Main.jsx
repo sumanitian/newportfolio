0050b83e9c5b22d8ecc076065e4d8de284ccaf94
sumanitian <sumanprasad26042001@gmail.com>
2023-09-25 20:53:48 +0530

    added new yt link

--- components/Main.jsx
+++ components/Main.jsx
@@ -59,11 +59,11 @@ const Main = () => {
                 <FaYoutube />
               </div>
             </a>
-            <Link href='/resume'>
+            {/* <Link href='/resume'>
               <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <BsFillPersonLinesFill />
               </div>
-            </Link>
+            </Link> */}
           </div>
         </div>
       </div>
--- components/youtube.jsx
+++ components/youtube.jsx
@@ -4,13 +4,13 @@ const HomePage = () => {
     return (
       <div id='youtube' className='w-full'>
         <div className='max-w-[1240px] mx-auto px-2 py-16'>
-            <p className='text-xl tracking-widest uppercase text-[#5651e5'>
+            <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
                 Youtube
             </p>
             <h2>Amazing Contents</h2>
             {/* <YouTubePlayer videoId="MLWznmAsfJk" /> */}
-            <div className='grid md:grid-cols-2 gap-8'>
-                <YouTubePlayer videoId="MLWznmAsfJk" />
+            <div className='grid md:grid-cols-4 gap-8'>
+                <YouTubePlayer videoId="DjQ69BQX8TQ" />
             </div>
         </div>
         
